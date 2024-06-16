ff271c034f058893fc98c0e03e14d84d59ff049d
James olujae <96151013+Jamesolujae123@users.noreply.github.com>
2024-06-16 08:32:22 -0700

    Update sigin.jsx

--- src/pages/sigin.jsx
+++ src/pages/sigin.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./signin.css";
+import { Link } from "react-router-dom";
 
 const Sigin = () => {
   return (
-    <div>Sigin</div>
-  )
-}
+    <div className="s-bk">
+      <div className="wrappert">
+        <h1 className="headd">Hello Again!</h1>
+        <p className="tooo">
+          Welcome back you've <br /> been missed!
+        </p>
+        <form className="bacc">
+          <input type="texttt" placeholder="Enter username" />
+          <input type="passwordd" placeholder="Password" />
+          <p className="recover">
+            <a className="tingww" href="#">
+              Recover Password
+            </a>
+          </p>
+        </form>
+        <button className="m">Sign in</button>
+        <p className="or">----- or continue with -----</p>
+        <div className="icons">
+          <i className="fab fa-google"></i>
+          <i className="fab fa-github"></i>
+          <i className="fab fa-facebook"></i>
+        </div>
+        <div className="not-member">
+          Not a member? <Link to={"/sign-up"}> Register Now</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sigin
+export default Sigin;
